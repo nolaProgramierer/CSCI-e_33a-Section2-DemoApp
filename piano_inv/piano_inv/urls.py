d63e5bb0d120928c0e7f299aaf6b4a5e1535364a
Glenn Langdon <glennlangdon@gmail.com>
2024-07-01 23:04:33 -0500

    add session functionality

--- piano_inv/piano_inv/urls.py
+++ piano_inv/piano_inv/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pianos/', include("pianos.urls"))
+    path('', include("pianos.urls"))
 ]
--- piano_inv/pianos/views.py
+++ piano_inv/pianos/views.py
@@ -3,44 +3,69 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django import forms
 
-pianos = [
-    {"brand": "Steinway", "price": 85000, "finish": "ebony"}
-]
+# pianos = [
+#     {"brand": "Steinway", "price": 85000, "finish": "ebony"}
+# ]
 
 class AddPianoForm(forms.Form):
-    brand = forms.CharField(label="Brand: ", max_length=50)
-    finish = forms.CharField(label="Finish: ", max_length=50)
-    price = forms.IntegerField(label="Price: ")
+    brand = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control flex-item',
+            'placeholder': "Piano brand",
+    })
+    )
+    finish = forms.CharField(
+         widget=forms.TextInput(attrs={
+            'class': 'form-control flex-item',
+            'placeholder': "Piano finish"
+    })
+    )
+    price = forms.IntegerField(
+         widget=forms.TextInput(attrs={
+            'class': 'form-control flex-item',
+            'placeholder': "Piano price"
+    })
+    )
 
 
 def index(request):
-    context = {"pianos": pianos}
-    return render(request, "pianos/index.html", context)
+    # request.session.flush()
+    pianos = request.session.get('pianos', [])
+    return render(request, "pianos/index.html", {"pianos": pianos})
 
 
 def add_piano(request):
     if request.method == "POST":
         # Add the request data to a blank form
         form = AddPianoForm(request.POST)
+        
         # Server side validation
         if form.is_valid():
             # Request data is now contained in the django 'cleaned_data' attribute
             brand = form.cleaned_data["brand"]
             finish = form.cleaned_data["finish"]
             price = form.cleaned_data["price"]
+            
             # Combine form entries into python dictionary
             new_piano = {"brand": brand, "finish": finish, "price": price}
+
+            # Retrieve the pianos object from the session
+            pianos = request.session.get('pianos', [])
+            
             # Append to 'pianos' dictionary array
             pianos.append(new_piano)
+            # Print to terminal for testing 
             print(pianos)
+
+            # Add the new pianos object to the session
+            request.session['pianos'] = pianos
+            # Ensure the session is saved
+            request.session.modified = True
+
+            # Return the index page
             return HttpResponseRedirect(reverse("index"))
+        
         # If the form isn't valid render the form with invalid entries
         else: return render(request, "pianos/add_piano.html", {"form": form})
+    
     return render(request, "pianos/add_piano.html", { "form": AddPianoForm()})
-
-
-
-
-
-
-
